55f27bf363b461b1f89584fc5bb7c5bbe08f3874
Yuya Hirayama <hiragram@gmail.com>
2024-10-20 00:22:47 +0900

    Update highlighter

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,12 @@
 import "../styles/global.css";
-import 'prism-themes/themes/prism-dracula.css'; // Prism.jsのテーマCSSをインポート
-import 'prismjs'; // Prism.jsのメインスクリプト
-import 'prismjs/components/prism-javascript'; // 必要な言語のサポート
-import 'prismjs/components/prism-css'; // 必要に応じて他の言語もインポート
-import 'prismjs/components/prism-swift';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/kimbie-dark.css';
 import { useEffect } from 'react';
 
 
 export default function App({ Component, pageProps }) {
     useEffect(() => {
-        // Prism.jsを初期化してシンタックスハイライトを適用
-        Prism.highlightAll();
+        hljs.highlightAll();
     }, []);
 
     return <Component {...pageProps} />;
