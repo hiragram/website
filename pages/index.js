bc421b52268555d29a429a0d9e42f5398cdea80d
Yuya Hirayama <hiragram@gmail.com>
2024-11-22 21:32:59 +0900

    Sort by published date

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,26 @@ import Layout from '@/components/layout';
 import { formattedDate } from '@/utils/dateFormat';
 
 export default function Home({ posts }) {
-  return (
-    <Layout>
-        <div className="posts-container">
-            <h1>Posts <a href="/feed"><span className="material-symbols-outlined accessory-icon">rss_feed</span></a></h1>
-            {posts.map((post) => (
-                
-                <Link key={`${post.id}`} href={`/posts/${post.id}`}>
-                    <div className="post-container" key={post.id}>
-                        <p className="datetime">{formattedDate(post.publishedAt)}</p>
-                        <p className="post-title">{post.title}</p>
-                    </div>
-                </Link>
-                
-            ))}
+    // Sort posts by publishedAt date in descending order
+    const sortedPosts = posts.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
 
-            <p className="link-to-older-blog"><a target="_blank" href="https://hiragram.hatenablog.jp/archive">それ以前のブログ</a></p>
-        </div>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <div className="posts-container">
+                <h1>Posts <a href="/feed"><span className="material-symbols-outlined accessory-icon">rss_feed</span></a></h1>
+                {sortedPosts.map((post) => (
+                    <Link key={`${post.id}`} href={`/posts/${post.id}`}>
+                        <div className="post-container" key={post.id}>
+                            <p className="datetime">{formattedDate(post.publishedAt)}</p>
+                            <p className="post-title">{post.title}</p>
+                        </div>
+                    </Link>
+                ))}
+
+                <p className="link-to-older-blog"><a target="_blank" href="https://hiragram.hatenablog.jp/archive">それ以前のブログ</a></p>
+            </div>
+        </Layout>
+    );
 }
 
 export const getStaticProps = async () => {
