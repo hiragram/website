33d53260f028b8dbd54760bce6177ee7e774f3d4
Yuya Hirayama <hiragram@gmail.com>
2024-12-03 06:23:11 +0900

    Fix timezone issue (hydration error)

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,8 +1,15 @@
 // 日付をyyyy-mm-dd形式に変換する関数
 export const formattedDate = (dateString) => {
-    const date = new Date(dateString);
-    const year = date.getFullYear();
-    const month = (`0${date.getMonth() + 1}`).slice(-2); // 月は0から始まるため+1
-    const day = (`0${date.getDate()}`).slice(-2); // 日付が1桁のときに0埋めする
-    return `${year}-${month}-${day}`;
-  };
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = (`0${date.getMonth() + 1}`).slice(-2); // 月は0から始まるため+1
+  const day = (`0${date.getDate()}`).slice(-2); // 日付が1桁のときに0埋めする
+
+  // JSTに変換
+  const jstDate = new Date(date.toLocaleString('en-US', { timeZone: 'Asia/Tokyo' }));
+  const jstYear = jstDate.getFullYear();
+  const jstMonth = (`0${jstDate.getMonth() + 1}`).slice(-2);
+  const jstDay = (`0${jstDate.getDate()}`).slice(-2);
+
+  return `${jstYear}-${jstMonth}-${jstDay}`;
+};
