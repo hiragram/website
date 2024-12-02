5d5b60084a9801fc8e198a0cee8a51a0549eb6e1
Yuya Hirayama <hiragram@gmail.com>
2024-12-03 04:48:14 +0900

    Add GoogleAnalytics

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,14 +2,11 @@ import { ReactNode } from 'react';
 import Header from '@/components/header';
 import Head from 'next/head';
 
-import { Analytics } from "@vercel/analytics/react"
-
 type Props = { children: ReactNode }
 
 const Layout = ({ children }: Props) => {
     return (
         <div>
-            <Analytics />
             <Head>
                 <link rel="icon" href="/favicon.ico" />
             </Head> 
